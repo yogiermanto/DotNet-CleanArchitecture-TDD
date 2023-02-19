bcba691b81cfea202ce5fb28806475087a10415a
Yogi Ermanto <yogiermanto59@gmail.com>
2023-02-19 11:33:11 +0700

    Add RoomBookingService class

---
+++ RoomBookingApp.Persistence/Repositories/RoomBookingService.cs
@@ -0,0 +1,18 @@
+﻿using RoomBookingApp.Core.DataServices;
+using RoomBookingApp.Domain;
+
+namespace RoomBookingApp.Persistence.Repositories
+{
+    public class RoomBookingService : IRoomBookingService
+    {
+        public List<Room> GetAvailableRooms(DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(RoomBooking roomBooking)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
